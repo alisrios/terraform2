e9561f7bc3d836c1c3818ed9d380ff118feaa195
alisrios <alisrios@gmail.com>
2024-07-24 14:25:32 -0300

    adicionando service e ajuste de tf e asg

--- output.tf
+++ output.tf
@@ -32,3 +32,7 @@ output "bia_repo_url" {
   value = aws_ecr_repository.bia.repository_url
 }
   
+output "rds_secret_name" {
+  description = "Nome do meu segredo"
+  value = data.aws_secretsmanager_secret.bia_db.name
+}  
